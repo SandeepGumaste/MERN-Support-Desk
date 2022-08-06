e7a10fd78b23318d3fae92e2bf1bdb881b6a6372
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2022-08-07 00:50:51 +0530

    Jwt for user creation and login

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log("server");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+const PORT = process.env.PORT || 5000;
+
+// connect to db
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
+
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
