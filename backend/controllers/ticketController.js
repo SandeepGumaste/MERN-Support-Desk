e673a58e75d9838a9c692172fe993e0ceb5f9d16
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2022-08-16 01:30:03 +0530

    get and create tickets functionality added

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-const ticket = require("../models/ticketModel");
+const Ticket = require("../models/ticketModel");
 
 // @desc get user tickets
 //  @route GET /api/tickets
@@ -8,9 +8,14 @@ const ticket = require("../models/ticketModel");
 
 const getTickets = asyncHandler(async (req, res) => {
   // get User using id and jwt
-  res.status(200).json({
-    message: "getTickets",
-  });
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+  res.status(200).json(tickets);
 });
 
 // @desc get user tickets
@@ -18,9 +23,26 @@ const getTickets = asyncHandler(async (req, res) => {
 // @access private
 
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "createTickets",
+  const { product, description } = req.body;
+  if (!product || !description) {
+    res.status(400);
+    throw new Error("Please add product and description");
+  }
+
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: "new",
   });
+
+  res.status(201).json(ticket);
 });
 
 module.exports = { getTickets, createTicket };
