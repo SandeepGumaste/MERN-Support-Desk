bd7e413f2393a61b9bfabffa2e33d44541d78131
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2022-08-16 01:23:25 +0530

    imports updated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const { protect } = require("../middleware/authMiddleware");
+const router = express.Router();
+
 const {
   registerUser,
   loginUser,
   getMe,
 } = require("../controllers/userController");
-const { protect } = require("../middleware/authMiddleware");
-
-const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
